ce9fcb6ee3ebf44135b76e1bdc81f27537b04da2
Edward Jiwook Kim <kimjiwook0129@gmail.com>
2021-09-12 16:07:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -136,13 +136,13 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(
-                        yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
+                        yellow.x + yellow.height, yellow.y + yellow.width // 2 - 2, 10, 5)
                     yellow_bullets.append(bullet)
                     #BULLET_FIRE_SOUND.play()
 
                 if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(
-                        red.x, red.y + red.height//2 - 2, 10, 5)
+                        red.x, red.y + red.width // 2 - 2, 10, 5)
                     red_bullets.append(bullet)
                     #BULLET_FIRE_SOUND.play()
 
